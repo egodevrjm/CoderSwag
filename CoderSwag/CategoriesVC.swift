659c66e08058240037167564ecc6712360ddbca4
Ryan Morrison <egodev@icloud.com>
2018-08-25 21:54:19 +0100

    categories and custom cell

--- CoderSwag/CategoriesVC.swift
+++ CoderSwag/CategoriesVC.swift
@@ -14,9 +14,27 @@ class CategoriesVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        categoryTable.dataSource = self
+        categoryTable.delegate = self
     }
-
-
 }
 
+
+extension CategoriesVC: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.getCategories().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath) as? CategoryCell {
+            let category = DataService.instance.getCategories()[indexPath.row]
+            cell.updateViews(category: category)
+            return cell
+        } else {
+            return CategoryCell()
+        }
+    }
+    
+    
+}
--- CoderSwag/CategoryCell.swift
+++ CoderSwag/CategoryCell.swift
@@ -13,15 +13,10 @@ class CategoryCell: UITableViewCell {
     @IBOutlet weak var categoryImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+   
+    func updateViews(category: Category){
+        categoryImage.image = UIImage(named: category.imageName)
+        titleLabel.text = category.title
     }
 
 }

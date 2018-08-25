814245b739b3aba45ae2fcaf44fa54e7806e84e5
Ryan Morrison <egodev@icloud.com>
2018-08-25 22:14:48 +0100

    CollectionView

---
+++ CoderSwag/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  CoderSwag
+//
+//  Created by Ryan Morrison on 25/08/2018.
+//  Copyright © 2018 Ryan Morrison. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}

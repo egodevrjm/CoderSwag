3f42253771942123db0b640b2f7938060d4cc877
Ryan Morrison <egodev@icloud.com>
2018-08-25 21:22:10 +0100

    Singleton

---
+++ CoderSwag/DataService.swift
@@ -0,0 +1,13 @@
+//
+//  DataService.swift
+//  CoderSwag
+//
+//  Created by Ryan Morrison on 25/08/2018.
+//  Copyright © 2018 Ryan Morrison. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+}
